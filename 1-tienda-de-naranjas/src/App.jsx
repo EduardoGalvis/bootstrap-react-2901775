8c30293afb323b3bc21159b39c1c723ab448290b
Eduardo Galvis Arango <35054260+EduardoGalvis@users.noreply.github.com>
2024-10-23 07:33:09 -0500

    Tiendas React

---
+++ 1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return(<div>
+            <header>
+              <nav>
+              Menú de navegación
+              </nav>
+              </header>
+              <section>
+                Sección promos
+              </section>
+              <section>
+                Sección tienda
+              </section>
+              <footer>
+                Pie de página
+              </footer>
+        </div>)
+}
+
+export default App
